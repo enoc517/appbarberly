1ce67dea4b2279dadd974cdf7cdcaf14f7ca54ea
HectorGR07 <gomezh250@gmail.com>
2026-04-20 14:58:27 -0600

    Update register and welcome screen

--- lib/features/welcome/presentation/screens/welcome_screen.dart
+++ lib/features/welcome/presentation/screens/welcome_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../../../shared/theme/app_theme.dart';
 import '../bloc/welcome_bloc.dart';
 import '../bloc/welcome_event.dart';
 import '../bloc/welcome_state.dart';
